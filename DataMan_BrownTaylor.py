a8b96c6501bd9644031c5806754c69e2e609fd78
TaylorBrown96 <78773029+TaylorBrown96@users.noreply.github.com>
2021-10-12 02:28:25 -0400

    Sprint 2
    Refer to Sprint 2 information in the header DocString.
    
    - Refactored regular_calculator
    - Added answer_checker

--- DataMan_BrownTaylor.py
+++ DataMan_BrownTaylor.py
@@ -3,7 +3,12 @@
 # 11OCT21
 
 """
-Sprint 1 -     
+_______________________________________________________________________________
+
+______Sprint 1______ 
+
+-New Content-
+   
 Main Menu:
 -----------
     Placeholder functions and a menu displayed with working paths.
@@ -13,9 +18,38 @@ Basic Calculator:
     The calculator works with the operands"+, -, /, *" and passes the entered 
     equation through an if-elif-else structure then displays the answer to 
     the entered equation using the choosen operand entered.
+_______________________________________________________________________________
+
+______Sprint 2______
+
+Basic Calculator
+-----------------
+    refactored this function, the processing component to a new function named 
+    calculator_core. Now sends the string to the function and recives a tuple 
+    to unpack and display.
+
+-New Content-
+
+Answer Checker
+----------------
+    The user is displayed with 3 options.
+    
+    -"User made questions" where the user inputs ten equations and 
+     then is asked to solve them. At the end the user is prompted with how many
+     correct and incorrect answers out of ten.
+     
+    -"Ten random questions" where the user will be prompted with ten random 
+     question and they will have 3 chances to get it right before the answer 
+     is given, at the end of the ten questions the user will get his/her score
+     out of ten.
+     
+    -"Exit" brings the user back to the main menu.
+_______________________________________________________________________________
 """
 
 def main():
+    """ Basic menu to send the user to other functions. """
+    
     keep_going = True
     
     while keep_going == True:
@@ -24,83 +58,160 @@ def main():
         usr_inp = input("Please Enter choice: ")
         
         if usr_inp == "1":
-            reg_cal() 
+            regular_calculator()
         elif usr_inp == "2":
-            ans_chker()
+            answer_checker()
         elif usr_inp == "3":
-            mem_bank()
+            memory_bank()
         elif usr_inp == "4":
-            num_gesr()
+            number_guesser()
         elif usr_inp == "5":
             print("Goodbye")
             keep_going = False
         else:
             print("Please choose a valid option")
             
-def reg_cal():
+def regular_calculator():
     """
-    - Determines the type of equation entered by the operand seperating
-        the two numbers entered.
-    - Delete the operator from the string.
-    - Split the equation into a list.
-    - Preform appropriate process to equation and then display results.
+    - Gathers equation information and sends it to the calculator_core function.
+    - Gets decoded answer back and splits the list apart.
+    - If the list length is not greater than 0 the function aborts.
+    - Prints out the solved equation.
     """
-    
     print("\nNormal Calculator-")
     keep_going = True
     
     while keep_going == True:
-        split_equate = []
         equate = input("Enter an equation or enter exit: ")
+        val_print = calculator_core(equate)
+        if len(val_print) > 0:
+            x,y,z = val_print[0],val_print[1],val_print[2]
+            operator = val_print[3]
+            print(x, operator, y, "=", z)
+        else:
+            break
+
+def answer_checker():
+    """ See DocString at top (Sprint 2: Answer Checker)"""
     
-    #Addition
-        if "+" in equate:
-          split_equate = equate.split("+")
-          x = int(split_equate[0])
-          y = int(split_equate[1])
-          z = x + y
-          print(x,"+",y,"=",z)
+    equation_list = []
+    answer_list = []
+    keep_going = True
+    index = 0
+    count = 0
+    correct = 0
+    incorrect = 0
+    list_length = 1
+    
+    while keep_going == True:
+        print("\nAnswer Checker")
+        print("1. User made questions\n2. Ten random questions\n3. Exit")
+        usr_inp = input("Enter choice here: ")
         
-    #Subtraction
-        elif "-" in equate:
-          split_equate = equate.split("-")
-          x = int(split_equate[0])
-          y = int(split_equate[1])
-          z = x - y
-          print(x,"-",y,"=",z)
-         
-    #Division  
-        elif "/" in equate:
-          split_equate = equate.split("/")
-          x = int(split_equate[0])
-          y = int(split_equate[1])
-          try:
-              z = x / y
-              print(x,"/",y,"=",z)
-          except ZeroDivisionError:
-              print("You cannot divide by zero!")
-              
-    #Multiplication
-        elif "*" in equate:
-          split_equate = equate.split("*")
-          x = int(split_equate[0])
-          y = int(split_equate[1])
-          z = x * y
-          print(x,"*",y,"=",z)
-          
-        elif equate == "exit" or "quit":
+        if usr_inp == "1":
+            print("Please enter 10 equations with the format: Number1+Number2")
+            for _ in range(10):
+                x = input(">")
+                equation_list.append(x)
+                answer = calculator_core(x)
+                answer_list.append(answer[2])
+            
+            while list_length > 0:
+                list_length = len(answer_list)
+                while list_length > 0:
+                    print(equation_list[index],"= ?")
+                    i = int(input("Answer: "))
+                    if i == answer_list[0]:
+                        print("That is correct!")
+                        correct += 1
+                        index += 1
+                        del answer_list[0]
+                        break
+                    elif count == 2:
+                        print("Sorry but the answer was:", answer_list[0],"\n")
+                        incorrect += 1
+                        count = 0
+                        index += 1
+                        del answer_list[0]
+                    elif i != answer_list[0]:
+                        print("Sorry that was incorrect")
+                        count += 1
+                            
+            print("\nYou got", correct, "correct and", incorrect, "incorrect out of 10")
+            
+        elif usr_inp == "2":
+            pass
+        elif usr_inp == "3":
             keep_going = False
         else:
-            print("That is not a valid equation.")
-
-def ans_chker():
-    print("Answer Checker")
-
-def mem_bank():
+            print("Please choose a valid option!")
+    
+def memory_bank():
     print("Memory Bank")
 
-def num_gesr():
+def number_guesser():
     print("Number Guesser")
+    
+def calculator_core(equate):
+    """
+     - Determines the type of equation entered by the operand seperating
+         the two numbers entered.
+     - Delete the operator from the string.
+     - Split the equation into a list.
+     - Preform appropriate process to equation and then return the results 
+         in a tuple.
+    """
+    split_equate = []
+    return_L = []
+    
+# Addition
+    if "+" in equate:
+      split_equate = equate.split("+")
+      x = int(split_equate[0])
+      y = int(split_equate[1])
+      z = x + y
+      return_L.extend((x,y,z))
+      return_L.append("+")
+      return return_L
+      
+# Subtraction
+    elif "-" in equate:
+      split_equate = equate.split("-")
+      x = int(split_equate[0])
+      y = int(split_equate[1])
+      z = x - y
+      return_L.extend((x,y,z))
+      return_L.append("-")
+      return return_L
+        
+# Division  
+    elif "/" in equate:
+      split_equate = equate.split("/")
+      x = int(split_equate[0])
+      y = int(split_equate[1])
+      try:
+          z = x / y
+          return_L.extend((x,y,z))
+          return_L.append("/")
+          return return_L
+      except ZeroDivisionError:
+          print("You cannot divide by zero!")
+      return
+    
+# Multiplication
+    elif "*" in equate:
+      split_equate = equate.split("*")
+      x = int(split_equate[0])
+      y = int(split_equate[1])
+      z = x * y
+      return_L.extend((x,y,z))
+      return_L.append("+")
+      return return_L
+      
+    elif equate == "exit" or "quit":
+       return return_L
+    else:
+        print("That is not a valid equation.")
 
 if __name__ == "__main__":
     main()
